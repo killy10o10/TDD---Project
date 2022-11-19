d7a44652af784b83932c841c692e8583ed122269
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-11-19 09:53:48 +0000

    Add factorial method

---
+++ factorial.rb
@@ -0,0 +1,6 @@
+def factorial(n)
+  if n <= 1
+    return 1
+  end
+  n * factorial(n -1) 
+end
--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe 'Testing for Factorial' do
   it 'return a factorial of 4 to be 24' do
     expect(factorial(4)).to eql(24)
